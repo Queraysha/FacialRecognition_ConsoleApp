21bda2c301660e581fb2e4d4da899b407afa4d3a
a939274 <a939274@LAPTOP-EDM9E5LN>
2025-03-13 08:58:42 +0200

    Add project files.

---
+++ HomeForm.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FacialRecognition
+{
+    public partial class Form1: Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNext_Click(object sender, EventArgs e)
+        {
+            // Open the Report Issues form (Raza,2021)
+            AdminLoginForm LoginForm = new AdminLoginForm();
+            LoginForm.Show();
+            this.Hide(); // Hide Main Menu Form
+        }
+    }
+}
