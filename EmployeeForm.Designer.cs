a538bdc0172255493e9c2226e30f8168e3548d5a
a939274 <a939274@LAPTOP-EDM9E5LN>
2025-03-27 10:14:07 +0200

    Employee form updated

--- EmployeeForm.Designer.cs
+++ EmployeeForm.Designer.cs
@@ -28,73 +28,76 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
+            this.dgvEmployee = new System.Windows.Forms.DataGridView();
+            this.btnAccept = new System.Windows.Forms.Button();
+            this.btnDecline = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.btnBack = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployee)).BeginInit();
             this.SuspendLayout();
             // 
-            // dataGridView1
+            // dgvEmployee
             // 
-            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(48, 238);
-            this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.RowHeadersWidth = 51;
-            this.dataGridView1.RowTemplate.Height = 24;
-            this.dataGridView1.Size = new System.Drawing.Size(704, 213);
-            this.dataGridView1.TabIndex = 2;
+            this.dgvEmployee.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEmployee.Location = new System.Drawing.Point(48, 238);
+            this.dgvEmployee.Name = "dgvEmployee";
+            this.dgvEmployee.RowHeadersWidth = 51;
+            this.dgvEmployee.RowTemplate.Height = 24;
+            this.dgvEmployee.Size = new System.Drawing.Size(765, 209);
+            this.dgvEmployee.TabIndex = 2;
             // 
-            // button1
+            // btnAccept
             // 
-            this.button1.Location = new System.Drawing.Point(64, 148);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 3;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnAccept.Location = new System.Drawing.Point(204, 148);
+            this.btnAccept.Name = "btnAccept";
+            this.btnAccept.Size = new System.Drawing.Size(75, 23);
+            this.btnAccept.TabIndex = 3;
+            this.btnAccept.Text = "Accept ";
+            this.btnAccept.UseVisualStyleBackColor = true;
+            this.btnAccept.Click += new System.EventHandler(this.btnAccept_Click);
             // 
-            // button2
+            // btnDecline
             // 
-            this.button2.Location = new System.Drawing.Point(677, 148);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnDecline.Location = new System.Drawing.Point(579, 138);
+            this.btnDecline.Name = "btnDecline";
+            this.btnDecline.Size = new System.Drawing.Size(75, 23);
+            this.btnDecline.TabIndex = 4;
+            this.btnDecline.Text = "Decline User";
+            this.btnDecline.UseVisualStyleBackColor = true;
+            this.btnDecline.Click += new System.EventHandler(this.btnDecline_Click_1);
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(387, 61);
+            this.label1.Location = new System.Drawing.Point(367, 89);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(44, 16);
+            this.label1.Size = new System.Drawing.Size(130, 16);
             this.label1.TabIndex = 5;
-            this.label1.Text = "label1";
+            this.label1.Text = "Employee Requests";
             // 
             // btnBack
             // 
-            this.btnBack.Location = new System.Drawing.Point(677, 486);
+            this.btnBack.Location = new System.Drawing.Point(579, 480);
             this.btnBack.Name = "btnBack";
             this.btnBack.Size = new System.Drawing.Size(75, 23);
             this.btnBack.TabIndex = 6;
             this.btnBack.Text = "Back";
             this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
             // EmployeeForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 537);
+            this.ClientSize = new System.Drawing.Size(847, 575);
             this.Controls.Add(this.btnBack);
             this.Controls.Add(this.label1);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnDecline);
+            this.Controls.Add(this.btnAccept);
+            this.Controls.Add(this.dgvEmployee);
             this.Name = "EmployeeForm";
             this.Text = "EmployeeForm";
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployee)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -102,9 +105,9 @@
 
         #endregion
 
-        private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dgvEmployee;
+        private System.Windows.Forms.Button btnAccept;
+        private System.Windows.Forms.Button btnDecline;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button btnBack;
     }
--- EmployeeForm.cs
+++ EmployeeForm.cs
@@ -2,19 +2,121 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ZXing;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace FacialRecognition
 {
     public partial class EmployeeForm: Form
     {
+        //SqlConnection conn = new SqlConnection(@"Data Source=LAPTOP-EDM9E5LN;Initial Catalog=VisitorManagementDB;Integrated Security=True;");
+
+        // private string connectionString = "your_connection_string_here"; // Replace with your database connection string
+        string connectionString = @"Data Source= LAPTOP-EDM9E5LN;Initial Catalog=VisitorManagementDB;Integrated Security=True";
+
         public EmployeeForm()
         {
-            InitializeComponent();
+            InitializeComponent(); 
+            LoadEmployees();
+        }
+
+        private void LoadEmployees()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT * FROM RequestTable WHERE Status = 'Employee'";
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dgvEmployee.DataSource = dt;
+            }
+        }
+
+        private void btnAccept_Click(object sender, EventArgs e)
+        {
+            if (dgvEmployee.CurrentRow != null)
+            {
+                int id = Convert.ToInt32(dgvEmployee.CurrentRow.Cells["ID"].Value);
+                AcceptUser(id);
+            }
+            ;
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            // Open the Main Menu Form form (Raza,2021)
+            MainMenuForm mm = new MainMenuForm();
+            mm.Show();
+            this.Hide(); // Hide Admin Form
+        }
+
+
+        private void btnDecline_Click_1(object sender, EventArgs e)
+        {
+            if (dgvEmployee.CurrentRow != null)
+            {
+                int id = Convert.ToInt32(dgvEmployee.CurrentRow.Cells["ID"].Value);
+                DeclineUser(id);
+            }
+        }
+
+        private void AcceptUser(int id)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlTransaction transaction = conn.BeginTransaction();  // Start transaction
+
+                try
+                {
+                    // Insert selected user into AcceptedTable
+                    string insertQuery = @"INSERT INTO AcceptedTable (Status, Name, Surname, Email, Password, DateOfEntry, PhoneNumber, QRCode, Image)
+                                   SELECT Status, Name, Surname, Email, Password, DateOfEntry, PhoneNumber, QRCode, Image
+                                   FROM RequestTable WHERE ID = @ID";
+
+                    SqlCommand insertCmd = new SqlCommand(insertQuery, conn, transaction);
+                    insertCmd.Parameters.AddWithValue("@ID", id);
+                    insertCmd.ExecuteNonQuery();
+
+                    // Delete the user from RequestTable
+                    string deleteQuery = "DELETE FROM RequestTable WHERE ID = @ID";
+                    SqlCommand deleteCmd = new SqlCommand(deleteQuery, conn, transaction);
+                    deleteCmd.Parameters.AddWithValue("@ID", id);
+                    deleteCmd.ExecuteNonQuery();
+
+                    transaction.Commit(); // Commit transaction
+                    MessageBox.Show("User Accepted and Moved Successfully!");
+                    LoadEmployees(); // Refresh the grid
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback(); // Rollback on failure
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void DeclineUser(int id)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "DELETE FROM RequestTable WHERE ID = @ID";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@ID", id);
+                cmd.ExecuteNonQuery();
+                LoadEmployees();
+                MessageBox.Show("User Declined!");
+            }
         }
     }
 }
