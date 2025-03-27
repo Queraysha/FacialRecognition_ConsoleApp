4c143246b4f5de36c4f87fe39fe5e1b4629b504c
a939274 <a939274@LAPTOP-EDM9E5LN>
2025-03-27 10:14:57 +0200

    Created update User Form

---
+++ UpdateUserForm.Designer.cs
@@ -0,0 +1,264 @@
+﻿namespace ManageStoredUsersForm.cs
+{
+    partial class UpdateUserForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtSurname = new System.Windows.Forms.TextBox();
+            this.lblName = new System.Windows.Forms.Label();
+            this.lblLastName = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblDepartment = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.cmbStatus = new System.Windows.Forms.ComboBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPhoneNumber = new System.Windows.Forms.TextBox();
+            this.dtpEntryDate = new System.Windows.Forms.MonthCalendar();
+            this.lblEntryDate = new System.Windows.Forms.Label();
+            this.lblRID = new System.Windows.Forms.Label();
+            this.txtRID = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtName
+            // 
+            this.txtName.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtName.Location = new System.Drawing.Point(188, 117);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(167, 22);
+            this.txtName.TabIndex = 0;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtEmail.Location = new System.Drawing.Point(188, 241);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(167, 22);
+            this.txtEmail.TabIndex = 2;
+            // 
+            // txtSurname
+            // 
+            this.txtSurname.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtSurname.Location = new System.Drawing.Point(188, 182);
+            this.txtSurname.Name = "txtSurname";
+            this.txtSurname.Size = new System.Drawing.Size(167, 22);
+            this.txtSurname.TabIndex = 3;
+            // 
+            // lblName
+            // 
+            this.lblName.AutoSize = true;
+            this.lblName.BackColor = System.Drawing.Color.DodgerBlue;
+            this.lblName.Location = new System.Drawing.Point(32, 119);
+            this.lblName.Name = "lblName";
+            this.lblName.Size = new System.Drawing.Size(50, 16);
+            this.lblName.TabIndex = 4;
+            this.lblName.Text = " Name ";
+            // 
+            // lblLastName
+            // 
+            this.lblLastName.AutoSize = true;
+            this.lblLastName.Location = new System.Drawing.Point(32, 178);
+            this.lblLastName.Name = "lblLastName";
+            this.lblLastName.Size = new System.Drawing.Size(61, 16);
+            this.lblLastName.TabIndex = 5;
+            this.lblLastName.Text = "Surname";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.AutoSize = true;
+            this.lblEmail.Location = new System.Drawing.Point(32, 243);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(98, 16);
+            this.lblEmail.TabIndex = 6;
+            this.lblEmail.Text = "Email Address ";
+            // 
+            // lblDepartment
+            // 
+            this.lblDepartment.AutoSize = true;
+            this.lblDepartment.Location = new System.Drawing.Point(32, 313);
+            this.lblDepartment.Name = "lblDepartment";
+            this.lblDepartment.Size = new System.Drawing.Size(44, 16);
+            this.lblDepartment.TabIndex = 7;
+            this.lblDepartment.Text = "Status";
+            // 
+            // btnSave
+            // 
+            this.btnSave.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnSave.Location = new System.Drawing.Point(589, 26);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(177, 77);
+            this.btnSave.TabIndex = 8;
+            this.btnSave.Text = "SAVE";
+            this.btnSave.UseVisualStyleBackColor = false;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnCancel.Location = new System.Drawing.Point(589, 127);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(177, 77);
+            this.btnCancel.TabIndex = 9;
+            this.btnCancel.Text = "CANCEL";
+            this.btnCancel.UseVisualStyleBackColor = false;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // cmbStatus
+            // 
+            this.cmbStatus.FormattingEnabled = true;
+            this.cmbStatus.Location = new System.Drawing.Point(188, 305);
+            this.cmbStatus.Name = "cmbStatus";
+            this.cmbStatus.Size = new System.Drawing.Size(167, 24);
+            this.cmbStatus.TabIndex = 10;
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(32, 369);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(67, 16);
+            this.lblPassword.TabIndex = 11;
+            this.lblPassword.Text = "Password";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPassword.Location = new System.Drawing.Point(188, 367);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(167, 22);
+            this.txtPassword.TabIndex = 12;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 425);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 16);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Phone Number";
+            // 
+            // txtPhoneNumber
+            // 
+            this.txtPhoneNumber.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtPhoneNumber.Location = new System.Drawing.Point(188, 418);
+            this.txtPhoneNumber.Name = "txtPhoneNumber";
+            this.txtPhoneNumber.Size = new System.Drawing.Size(167, 22);
+            this.txtPhoneNumber.TabIndex = 14;
+            // 
+            // dtpEntryDate
+            // 
+            this.dtpEntryDate.Location = new System.Drawing.Point(489, 233);
+            this.dtpEntryDate.Name = "dtpEntryDate";
+            this.dtpEntryDate.TabIndex = 15;
+            this.dtpEntryDate.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.dtpEntryDate_DateChanged);
+            // 
+            // lblEntryDate
+            // 
+            this.lblEntryDate.AutoSize = true;
+            this.lblEntryDate.Location = new System.Drawing.Point(396, 339);
+            this.lblEntryDate.Name = "lblEntryDate";
+            this.lblEntryDate.Size = new System.Drawing.Size(69, 16);
+            this.lblEntryDate.TabIndex = 16;
+            this.lblEntryDate.Text = "Entry Date";
+            // 
+            // lblRID
+            // 
+            this.lblRID.AutoSize = true;
+            this.lblRID.BackColor = System.Drawing.Color.DodgerBlue;
+            this.lblRID.Location = new System.Drawing.Point(32, 56);
+            this.lblRID.Name = "lblRID";
+            this.lblRID.Size = new System.Drawing.Size(30, 16);
+            this.lblRID.TabIndex = 17;
+            this.lblRID.Text = "RID";
+            // 
+            // txtRID
+            // 
+            this.txtRID.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtRID.Location = new System.Drawing.Point(188, 50);
+            this.txtRID.Name = "txtRID";
+            this.txtRID.Size = new System.Drawing.Size(167, 22);
+            this.txtRID.TabIndex = 18;
+            // 
+            // UpdateUserForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.DodgerBlue;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtRID);
+            this.Controls.Add(this.lblRID);
+            this.Controls.Add(this.lblEntryDate);
+            this.Controls.Add(this.dtpEntryDate);
+            this.Controls.Add(this.txtPhoneNumber);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.cmbStatus);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.lblDepartment);
+            this.Controls.Add(this.lblEmail);
+            this.Controls.Add(this.lblLastName);
+            this.Controls.Add(this.lblName);
+            this.Controls.Add(this.txtSurname);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtName);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Name = "UpdateUserForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Update User";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtSurname;
+        private System.Windows.Forms.Label lblName;
+        private System.Windows.Forms.Label lblLastName;
+        private System.Windows.Forms.Label lblEmail;
+        private System.Windows.Forms.Label lblDepartment;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.ComboBox cmbStatus;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPhoneNumber;
+        private System.Windows.Forms.MonthCalendar dtpEntryDate;
+        private System.Windows.Forms.Label lblEntryDate;
+        private System.Windows.Forms.Label lblRID;
+        private System.Windows.Forms.TextBox txtRID;
+    }
+}
---
+++ UpdateUserForm.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ManageStoredUsersForm.cs
+{
+    public partial class UpdateUserForm : Form
+    {
+        private int _userId;
+        private string _connectionString = "Data Source=lindiwe;Initial Catalog=YourDatabase;Integrated Security=True";
+        private string _qrCode;
+        private string _imagePath;
+
+        public UpdateUserForm(int userId, string rid, string status, string name, string surname, string email,
+                             string password, DateTime? entryDate, string phoneNumber, string qrCode, string imagePath)
+        {
+            InitializeComponent();
+
+            _userId = userId;
+            _qrCode = qrCode;
+            _imagePath = imagePath;
+
+            // Populate the status (vis/emp) dropdown
+            cmbStatus.Items.Add("vis");
+            cmbStatus.Items.Add("emp");
+
+            // Set the current status
+            cmbStatus.SelectedItem = status;
+
+            // Populate form fields with user data
+
+
+            txtRID.Text = rid;
+            txtName.Text = name;
+            txtSurname.Text = surname;
+            txtEmail.Text = email;
+            txtPassword.Text = password;
+            txtPhoneNumber.Text = phoneNumber;
+
+            if (entryDate.HasValue)
+            {
+                dtpEntryDate.MaxDate = entryDate.Value;
+            }
+            else
+            {
+                dtpEntryDate.MaxDate = DateTime.Now;
+            }
+
+            // Update field visibility based on status (vis/emp)
+            UpdateFieldVisibility();
+        }
+
+        private void UpdateFieldVisibility()
+        {
+            string status = cmbStatus.SelectedItem?.ToString();
+
+            // Password only for employees
+            lblPassword.Visible = status == "emp";
+            txtPassword.Visible = status == "emp";
+
+            // Entry date only for visitors
+            lblEntryDate.Visible = status == "vis";
+            dtpEntryDate.Visible = status == "vis";
+            dtpEntryDate.Visible = status == "vis"; // Show calendar only for visitors
+        }
+
+        private void cmbStatus_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            UpdateFieldVisibility();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                // Validate required fields
+                if (
+                    string.IsNullOrWhiteSpace(txtName.Text) ||
+                    string.IsNullOrWhiteSpace(txtSurname.Text) ||
+                    string.IsNullOrWhiteSpace(txtEmail.Text) ||
+                    cmbStatus.SelectedItem == null)
+                {
+                    MessageBox.Show("Please fill in all required fields.", "Validation Error",
+                                   MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                // Additional validation for employees (must have password)
+                if (cmbStatus.SelectedItem.ToString() == "emp" && string.IsNullOrWhiteSpace(txtPassword.Text))
+                {
+                    MessageBox.Show("Password is required for employees.", "Validation Error",
+                                   MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    return;
+                }
+
+                using (SqlConnection connection = new SqlConnection(_connectionString))
+                {
+                    connection.Open();
+
+                    string query = @"UPDATE StoredUsers SET 
+                                     RID = @RID, 
+                                    Status = @Status, 
+                                    Name = @Name, 
+                                    Surname = @Surname, 
+                                    Email = @Email, 
+                                    Password = @Password, 
+                                    EntryDate = @EntryDate,
+                                    PhoneNumber = @PhoneNumber
+                                    WHERE Id = @Id";
+
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@RID", txtRID.Text);
+                        command.Parameters.AddWithValue("@Status", cmbStatus.SelectedItem.ToString());
+                        command.Parameters.AddWithValue("@Name", txtName.Text);
+                        command.Parameters.AddWithValue("@Surname", txtSurname.Text);
+                        command.Parameters.AddWithValue("@Email", txtEmail.Text);
+
+                        // Handle conditional fields
+                        if (cmbStatus.SelectedItem.ToString() == "emp")
+                        {
+                            command.Parameters.AddWithValue("@Password", txtPassword.Text);
+                            command.Parameters.AddWithValue("@EntryDate", DBNull.Value);
+                        }
+                        else // visitor
+                        {
+                            command.Parameters.AddWithValue("@Password", DBNull.Value);
+                            command.Parameters.AddWithValue("@EntryDate", dtpEntryDate.MaxDate);
+                        }
+
+                        // Handle optional field
+                        if (string.IsNullOrWhiteSpace(txtPhoneNumber.Text))
+                        {
+                            command.Parameters.AddWithValue("@PhoneNumber", DBNull.Value);
+                        }
+                        else
+                        {
+                            command.Parameters.AddWithValue("@PhoneNumber", txtPhoneNumber.Text);
+                        }
+
+                        command.Parameters.AddWithValue("@Id", _userId);
+
+                        int rowsAffected = command.ExecuteNonQuery();
+
+                        if (rowsAffected > 0)
+                        {
+                            MessageBox.Show("User updated successfully!", "Success",
+                                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                            DialogResult = DialogResult.OK;
+                            Close();
+                        }
+                        else
+                        {
+                            MessageBox.Show("No changes were made.", "Information",
+                                MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error updating user: {ex.Message}", "Error",
+                    MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            DialogResult = DialogResult.Cancel;
+            Close();
+        }
+
+        private void dtpEntryDate_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            // This code will run when the user selects a different date
+            DateTime selectedDate = dtpEntryDate.SelectionStart;
+            MessageBox.Show($"Selected date: {selectedDate.ToShortDateString()}");
+
+            dtpEntryDate.Visible = !dtpEntryDate.Visible;
+            if (dtpEntryDate.Visible)
+            {
+                // Position the calendar near the date time picker if needed
+                dtpEntryDate.Location = new Point(dtpEntryDate.Location.X,
+                                                  dtpEntryDate.Location.Y + dtpEntryDate.Height);
+            }
+
+
+
+
+        }
+    }
+}
