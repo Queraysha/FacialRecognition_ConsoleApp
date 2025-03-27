b342d5c17a80d9cbd1641ac20cdb0b4452467572
a939274 <a939274@LAPTOP-EDM9E5LN>
2025-03-27 10:13:17 +0200

    Created Accepted Users form

---
+++ AcceptedUsersForm.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace FacialRecognition
+{
+    partial class AcceptedUsersForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.dgvEmployee = new System.Windows.Forms.DataGridView();
+            this.dgvVisitors = new System.Windows.Forms.DataGridView();
+            this.btnRemove = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployee)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVisitors)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnUpdate.BackColor = System.Drawing.Color.Blue;
+            this.btnUpdate.Location = new System.Drawing.Point(3, 47);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(121, 60);
+            this.btnUpdate.TabIndex = 2;
+            this.btnUpdate.Text = "UPDATE";
+            this.btnUpdate.UseVisualStyleBackColor = false;
+            this.btnUpdate.Click += new System.EventHandler(this.btnUpdate_Click);
+            // 
+            // dgvEmployee
+            // 
+            this.dgvEmployee.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvEmployee.Location = new System.Drawing.Point(9, 148);
+            this.dgvEmployee.Name = "dgvEmployee";
+            this.dgvEmployee.RowHeadersWidth = 51;
+            this.dgvEmployee.RowTemplate.Height = 24;
+            this.dgvEmployee.Size = new System.Drawing.Size(1315, 221);
+            this.dgvEmployee.TabIndex = 3;
+            // 
+            // dgvVisitors
+            // 
+            this.dgvVisitors.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvVisitors.Location = new System.Drawing.Point(15, 443);
+            this.dgvVisitors.Name = "dgvVisitors";
+            this.dgvVisitors.RowHeadersWidth = 51;
+            this.dgvVisitors.RowTemplate.Height = 24;
+            this.dgvVisitors.Size = new System.Drawing.Size(1315, 226);
+            this.dgvVisitors.TabIndex = 4;
+            // 
+            // btnRemove
+            // 
+            this.btnRemove.Anchor = System.Windows.Forms.AnchorStyles.Bottom;
+            this.btnRemove.BackColor = System.Drawing.Color.Blue;
+            this.btnRemove.Location = new System.Drawing.Point(254, 47);
+            this.btnRemove.Name = "btnRemove";
+            this.btnRemove.Size = new System.Drawing.Size(121, 60);
+            this.btnRemove.TabIndex = 5;
+            this.btnRemove.Text = "DELETE";
+            this.btnRemove.UseVisualStyleBackColor = false;
+            this.btnRemove.Click += new System.EventHandler(this.btnRemove_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnClose.BackColor = System.Drawing.Color.DarkOrange;
+            this.btnClose.Location = new System.Drawing.Point(952, 704);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(378, 60);
+            this.btnClose.TabIndex = 6;
+            this.btnClose.Text = "CLOSE";
+            this.btnClose.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(621, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 16);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "Accepted Upsers";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(76, 16);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "Employees";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 408);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 16);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Visitors";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.BackColor = System.Drawing.Color.SkyBlue;
+            this.label4.Location = new System.Drawing.Point(147, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 16);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Filter Users";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DeepSkyBlue;
+            this.panel1.Controls.Add(this.label4);
+            this.panel1.Controls.Add(this.btnUpdate);
+            this.panel1.Controls.Add(this.btnRemove);
+            this.panel1.Location = new System.Drawing.Point(949, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(378, 114);
+            this.panel1.TabIndex = 11;
+            // 
+            // AcceptedUsersForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1339, 776);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.dgvVisitors);
+            this.Controls.Add(this.dgvEmployee);
+            this.Controls.Add(this.panel1);
+            this.Name = "AcceptedUsersForm";
+            this.Text = "AcceptedUsers";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvEmployee)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVisitors)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.DataGridView dgvEmployee;
+        private System.Windows.Forms.DataGridView dgvVisitors;
+        private System.Windows.Forms.Button btnRemove;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
---
+++ AcceptedUsersForm.cs
@@ -0,0 +1,141 @@
+﻿using ManageStoredUsersForm.cs;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Speech.Synthesis;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FacialRecognition
+{
+    public partial class AcceptedUsersForm: Form
+    {
+
+        private string connectionString = @"Data Source= LAPTOP-EDM9E5LN;Initial Catalog=VisitorManagementDB;Integrated Security=True";
+
+
+        public AcceptedUsersForm()
+        {
+            InitializeComponent();
+            LoadAcceptedUsers();
+        }
+        private void LoadAcceptedUsers()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+
+                // Load Employees
+                string employeeQuery = "SELECT * FROM AcceptedTable WHERE Status = 'Employee'";
+                SqlDataAdapter employeeAdapter = new SqlDataAdapter(employeeQuery, conn);
+                DataTable employeeTable = new DataTable();
+                employeeAdapter.Fill(employeeTable);
+                dgvEmployee.DataSource = employeeTable;
+
+                // Load Visitors
+                string visitorQuery = "SELECT * FROM AcceptedTable WHERE Status = 'Visitor'";
+                SqlDataAdapter visitorAdapter = new SqlDataAdapter(visitorQuery, conn);
+                DataTable visitorTable = new DataTable();
+                visitorAdapter.Fill(visitorTable);
+                dgvVisitors.DataSource = visitorTable;
+            }
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            if (dgvEmployee.CurrentRow != null)
+            {
+                int id = Convert.ToInt32(dgvEmployee.CurrentRow.Cells["ID"].Value);
+                UpdateUser(id);
+            }
+            else if (dgvVisitors.CurrentRow != null)
+            {
+                int id = Convert.ToInt32(dgvVisitors.CurrentRow.Cells["ID"].Value);
+                UpdateUser(id);
+            }
+        }
+
+        private void UpdateUser(int id)
+        {
+            string newName = Prompt.ShowDialog("Enter new name:", "Update User");
+            string newSurname = Prompt.ShowDialog("Enter new surname:", "Update User");
+
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "UPDATE AcceptedTable SET Name = @Name, Surname = @Surname WHERE ID = @ID";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@Name", newName);
+                cmd.Parameters.AddWithValue("@Surname", newSurname);
+                cmd.Parameters.AddWithValue("@ID", id);
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("User updated successfully!");
+                LoadAcceptedUsers();
+            }
+        }
+
+        private void btnRemove_Click(object sender, EventArgs e)
+        {
+            if (dgvEmployee.CurrentRow != null)
+            {
+                int id = Convert.ToInt32(dgvEmployee.CurrentRow.Cells["ID"].Value);
+                RemoveUser(id);
+            }
+            else if (dgvVisitors.CurrentRow != null)
+            {
+                int id = Convert.ToInt32(dgvVisitors.CurrentRow.Cells["ID"].Value);
+                RemoveUser(id);
+            }
+        }
+
+        private void RemoveUser(int id)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "DELETE FROM AcceptedTable WHERE ID = @ID";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@ID", id);
+                cmd.ExecuteNonQuery();
+
+                MessageBox.Show("User removed successfully!");
+                LoadAcceptedUsers();
+            }
+        }
+
+    }
+
+    public static class Prompt
+    {
+        public static string ShowDialog(string text, string caption)
+        {
+            Form prompt = new Form()
+            {
+                Width = 400,
+                Height = 150,
+                Text = caption
+            };
+
+            Label textLabel = new Label() { Left = 50, Top = 20, Text = text };
+            TextBox inputBox = new TextBox() { Left = 50, Top = 50, Width = 300 };
+            Button confirmation = new Button() { Text = "OK", Left = 250, Width = 100, Top = 80 };
+
+            confirmation.Click += (sender, e) => { prompt.Close(); };
+            prompt.Controls.Add(textLabel);
+            prompt.Controls.Add(inputBox);
+            prompt.Controls.Add(confirmation);
+            prompt.ShowDialog();
+
+            return inputBox.Text;
+        }
+    }
+}
+
