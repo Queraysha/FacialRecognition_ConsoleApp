dc549bec40aaa91040997066cc8161408bb038d4
a939274 <a939274@LAPTOP-EDM9E5LN>
2025-03-27 10:29:54 +0200

    create visitor form

--- VisitorForm.Designer.cs
+++ VisitorForm.Designer.cs
@@ -28,12 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnDecline = new System.Windows.Forms.Button();
+            this.btnAccept = new System.Windows.Forms.Button();
+            this.dgvVisitors = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVisitors)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(549, 409);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(75, 23);
+            this.btnBack.TabIndex = 11;
+            this.btnBack.Text = "Back";
+            this.btnBack.UseVisualStyleBackColor = true;
+            this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(337, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 16);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Visitors Requests";
+            // 
+            // btnDecline
+            // 
+            this.btnDecline.Location = new System.Drawing.Point(549, 67);
+            this.btnDecline.Name = "btnDecline";
+            this.btnDecline.Size = new System.Drawing.Size(75, 23);
+            this.btnDecline.TabIndex = 9;
+            this.btnDecline.Text = "Decline User";
+            this.btnDecline.UseVisualStyleBackColor = true;
+            this.btnDecline.Click += new System.EventHandler(this.btnDecline_Click);
+            // 
+            // btnAccept
+            // 
+            this.btnAccept.Location = new System.Drawing.Point(174, 77);
+            this.btnAccept.Name = "btnAccept";
+            this.btnAccept.Size = new System.Drawing.Size(75, 23);
+            this.btnAccept.TabIndex = 8;
+            this.btnAccept.Text = "Accept ";
+            this.btnAccept.UseVisualStyleBackColor = true;
+            this.btnAccept.Click += new System.EventHandler(this.btnAccept_Click);
+            // 
+            // dgvVisitors
+            // 
+            this.dgvVisitors.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvVisitors.Location = new System.Drawing.Point(18, 167);
+            this.dgvVisitors.Name = "dgvVisitors";
+            this.dgvVisitors.RowHeadersWidth = 51;
+            this.dgvVisitors.RowTemplate.Height = 24;
+            this.dgvVisitors.Size = new System.Drawing.Size(765, 209);
+            this.dgvVisitors.TabIndex = 7;
+            // 
+            // VisitorForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnDecline);
+            this.Controls.Add(this.btnAccept);
+            this.Controls.Add(this.dgvVisitors);
+            this.Name = "VisitorForm";
             this.Text = "VisitorForm";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVisitors)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnDecline;
+        private System.Windows.Forms.Button btnAccept;
+        private System.Windows.Forms.DataGridView dgvVisitors;
     }
 }
--- VisitorForm.cs
+++ VisitorForm.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,105 @@ namespace FacialRecognition
 {
     public partial class VisitorForm: Form
     {
+
+        string connectionString = @"Data Source= LAPTOP-EDM9E5LN;Initial Catalog=VisitorManagementDB;Integrated Security=True";
+
         public VisitorForm()
         {
             InitializeComponent();
+            LoadVisitors();
+        }
+
+        private void LoadVisitors()
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "SELECT * FROM RequestTable WHERE Status = 'Visitor'";
+                SqlDataAdapter da = new SqlDataAdapter(query, conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                dgvVisitors.DataSource = dt;
+            }
+        }
+
+        private void btnAccept_Click(object sender, EventArgs e)
+        {
+            if (dgvVisitors.CurrentRow != null)
+            {
+                int id = Convert.ToInt32(dgvVisitors.CurrentRow.Cells["ID"].Value);
+                AcceptUser(id);
+            }
+            ;
+        }
+
+        private void btnBack_Click(object sender, EventArgs e)
+        {
+            // Open the Main Menu Form form (Raza,2021)
+            MainMenuForm mm = new MainMenuForm();
+            mm.Show();
+            this.Hide(); // Hide Admin Form
+        }
+
+        private void AcceptUser(int id)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                SqlTransaction transaction = conn.BeginTransaction();  // Start transaction
+
+                try
+                {
+                    // Insert selected user into AcceptedTable
+                    string insertQuery = @"INSERT INTO AcceptedTable (Status, Name, Surname, Email, Password, DateOfEntry, PhoneNumber, QRCode, Image)
+                                   SELECT Status, Name, Surname, Email, Password, DateOfEntry, PhoneNumber, QRCode, Image
+                                   FROM RequestTable WHERE ID = @ID";
+
+                    SqlCommand insertCmd = new SqlCommand(insertQuery, conn, transaction);
+                    insertCmd.Parameters.AddWithValue("@ID", id);
+                    insertCmd.ExecuteNonQuery();
+
+                    // Delete the user from RequestTable
+                    string deleteQuery = "DELETE FROM RequestTable WHERE ID = @ID";
+                    SqlCommand deleteCmd = new SqlCommand(deleteQuery, conn, transaction);
+                    deleteCmd.Parameters.AddWithValue("@ID", id);
+                    deleteCmd.ExecuteNonQuery();
+
+                    transaction.Commit(); // Commit transaction
+                    MessageBox.Show("User Accepted and Moved Successfully!");
+                    LoadVisitors(); // Refresh the grid
+                }
+                catch (Exception ex)
+                {
+                    transaction.Rollback(); // Rollback on failure
+                    MessageBox.Show("Error: " + ex.Message);
+                }
+            }
+        }
+
+        private void DeclineUser(int id)
+        {
+            using (SqlConnection conn = new SqlConnection(connectionString))
+            {
+                conn.Open();
+                string query = "DELETE FROM RequestTable WHERE ID = @ID";
+                SqlCommand cmd = new SqlCommand(query, conn);
+                cmd.Parameters.AddWithValue("@ID", id);
+                cmd.ExecuteNonQuery();
+                LoadVisitors();
+                MessageBox.Show("User Declined!");
+            }
+        }
+
+        private void btnDecline_Click(object sender, EventArgs e)
+        {
+            if (dgvVisitors.CurrentRow != null)
+            {
+                int id = Convert.ToInt32(dgvVisitors.CurrentRow.Cells["ID"].Value);
+                DeclineUser(id);
+            }
+
         }
     }
 }
+
